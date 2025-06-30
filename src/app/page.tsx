9a5bc1582c0a770ac342950eefbaca6ae93e8b53
luzihao <2803998271@qq.com>
2025-06-30 23:25:29 +0800

    数据库

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   // const res = await fetch('http://localhost:4000/posts');
   // const data = await res.json();
   return (
-    <div>
+    <div>  
       djschsdbc
     </div>
   );
