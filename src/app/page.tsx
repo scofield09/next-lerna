7f39e60c0fe37ec43f51f28094b8004b69a9b9b5
luzihao <2803998271@qq.com>
2025-07-02 22:58:02 +0800

    连接数据库

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 // import { json } from "stream/consumers";
+import { neon } from "@neondatabase/serverless";
+const sql = neon(`${process.env.DATABASE_URL}`);
 
 export default async function Home() {
-  // const res = await fetch('http://localhost:4000/posts');
+  const result = await sql`SELECT * FROM users;`;
   // const data = await res.json();
   return (
     <div>  
-      djschsdbc
+      {result.map(item => <li key={item.id}>{item.username}</li>)}
     </div>
   );
 }
